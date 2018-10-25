c4adafbd923096c4f5e1668b91dc73b370a0bdca
League <League@AdminisorsiMac2.attlocal.net>
2018-10-24 19:51:55 -0700

    OOOOOOooooo

---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Free Vbucks");
+JOptionPane.showMessageDialog(null,"Welcome to Vbuck Generator");
+String username = JOptionPane.showInputDialog(" What is your Fortnite Username and Password");
+System.out.println(username);
+JOptionPane.showMessageDialog(null,"Your Vbucks have been sent to" + username);
+}
+}
+ 
